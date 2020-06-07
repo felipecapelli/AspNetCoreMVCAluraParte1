0c37aa4bde86d722f0a6870c59f19ae9d57b9db7
felipecapelli <felipe.ccapelli@gmail.com>
2020-06-07 19:34:45 -0300

    Aula 3

--- Aulas/Aula1/CasaDoCodigo/Startup.cs
+++ Aulas/Aula1/CasaDoCodigo/Startup.cs
@@ -4,28 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace CasaDoCodigo
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
+        public Startup(IConfiguration configuration){ Configuration = configuration;}
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            
+            //---------------------------------------------
+            string connectionString = Configuration.GetConnectionString("Default"); /*Esse Default foi configurado no arquivo appsettings*/
+            services.AddDbContext<ApplicationContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+            //---------------------------------------------
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -45,6 +51,9 @@ namespace CasaDoCodigo
                     name: "default",
                     template: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
+
+            //---------------- Criar o banco se não tiver criado
+            serviceProvider.GetService<ApplicationContext>().Database.Migrate(); //no final pode usar .EnSureCreated() no lugar de Migrate(), mas daí não vai poder mais fazer migrações
         }
     }
 }
